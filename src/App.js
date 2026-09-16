9910782b3555a3aa17c42e167a0774659b49f57f
gayeonnam812 <gayeonnam812@gmail.com>
2026-09-16 13:20:41 +0900

    day3-2

--- src/App.js
+++ src/App.js
@@ -1,10 +1,32 @@
 // import logo from './logo.svg';
 // import './App.css';
+import Greeting from './Greeting';
 
 function App() {
+
+  // logic
+  // 구조분해할당 - object destructuring
+  const animals = ["dog", "cat", "pig"];
+  // const second = animals[1];
+
+  const [ first, second ] = animals
+  // console.log("🚀 ~ App ~ first:", first)
+  // console.log("🚀 ~ App ~ second:", second)
+
+  const todoItems = {
+    todo: "react학습하기",
+    iscomplete: false
+  }
+
+  const {todo: todoName, iscomplete} = todoItems;
+  // console.log("🚀 ~ App ~ iscomplete:", iscomplete)
+  // console.log("🚀 ~ App ~ todoName:", todoName)
+
+  // view
   return (
     <div className="App">
       Hello World
+      <Greeting/>
     </div>
   );
 }
